7e7c14b27e699e489c76ee4f51b787e25876572a
wisnupramoedya <wisnupramoedya@gmail.com>
2021-09-29 22:26:48 +0700

    feat: update animation

---
+++ quiz1/profile/assets/script.js
@@ -0,0 +1,46 @@
+var generate = function (number) {
+    switch (number) {
+        case 1:
+            window.location.href = "https://www.php.net/";
+            break;
+        case 2:
+            window.location.href = "https://kotlinlang.org/";
+            break;
+        case 3:
+            window.location.href = "https://www.javascript.com/";
+            break;
+        case 4:
+            window.location.href = "https://www.java.com/en/";
+            break;
+        case 5:
+            window.location.href = "https://www.python.org/";
+            break;
+        case 6:
+            window.location.href = "https://laravel.com/";
+            break;
+        case 7:
+            window.location.href = "https://nodejs.org/en/";
+            break;
+        case 8:
+            window.location.href = "https://www.mysql.com/";
+            break;
+        case 9:
+            window.location.href = "https://www.postgresql.org/";
+            break;
+        case 10:
+            window.location.href = "https://www.android.com/";
+            break;
+        case 11:
+            window.location.href = "https://www.figma.com/";
+            break;
+        case 12:
+            window.location.href = "https://github.com/";
+            break;
+        case 13:
+            window.location.href = "https://www.docker.com/";
+            break;
+
+        default:
+            break;
+    }
+}
